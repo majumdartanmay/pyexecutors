8d2b814993f014a790ef563aa744810777e44cc8
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2019-11-04 00:25:15 +0530

    added access_providers in __init__.py

--- pyexecutors/__init__.py
+++ pyexecutors/__init__.py
@@ -12,4 +12,25 @@ from pyexecutors.holders.Tasks import AsyncTasks, SyncTasks
     
 """
 
-__version__ = "0.0.1.dev"
+
+def executor():
+    return Executors()
+
+
+def async_tasks(f, args=None, kwargs=None):
+    if args is None:
+        args = []
+    if kwargs is None:
+        kwargs = {}
+    return AsyncTasks(f, args, kwargs)
+
+
+def sync_tasks(f, args=None, kwargs=None):
+    if kwargs is None:
+        kwargs = {}
+    if args is None:
+        args = []
+    return SyncTasks(f, args, kwargs)
+
+
+__version__ = "0.0.3.dev"
