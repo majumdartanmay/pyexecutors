7bfd4f878d21e081f8f9dffa7b7cfa3b5d38703c
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2020-01-25 03:29:26 +0530

    license and docs updated

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='pyexecutors',
     packages=['pyexecutors', 'pyexecutors.executors', 'pyexecutors.holders', 'pyexecutors.utils'],
-    version='0.1.2',
+    version='0.1.3',
     license='MIT',  
     description=
     """
