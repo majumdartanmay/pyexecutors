9d1a0f9baca6a4b333d3a94426376a7cc3eb3b28
tanmay_majumdar <tanmay.majumdar@nowfloats.com>
2020-01-28 00:49:47 +0530

    polls added

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ with open(path.join(this_directory, 'README.md'), encoding='utf-8') as f:
 setup(
     name='pyexecutors',
     packages=['pyexecutors', 'pyexecutors.executors', 'pyexecutors.holders', 'pyexecutors.utils'],
-    version='0.1.5',
+    version='0.1.6',
     license='MIT',  
     description=
     """
